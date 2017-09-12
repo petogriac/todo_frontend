40124cff8bbb7082eb788ac20cf9263077d2b0a9
Peto Griac <peter.griac@gmail.com>
2017-09-12 08:45:28 +0200

    Initial commit with getting all todos prototype

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+
+    <h2>TODO APP</h2>
+
+    <ul id="example-1">
+      <li v-for="todo in todos">
+        {{ todo.todo }}
+
+        <i class="material-icons">edit</i>
+        <i class="material-icons">delete</i>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'app',
+  data(){
+      return {
+          todos: []
+      }
+  },
+  created() {
+      axios.get('http://localhost:3000/todos')
+          .then(response => this.todos = response.data)
+          .catch(error => console.log(error));
+  }
+}
+</script>
